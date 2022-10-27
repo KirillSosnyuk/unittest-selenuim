c2c5c68714298b13258bbae77956c7faabc4a413
Кирилл Соснюк <kirill-sosnyuk@yandex.ru>
2022-10-27 04:23:58 +0300

    Unused connections deleted

--- selenuim/auth_script.py
+++ selenuim/auth_script.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from langdetect import DetectorFactory, detect_langs
 import time
 import random
 
